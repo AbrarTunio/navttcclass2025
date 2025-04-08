39c7de50791604d3bd85a0790354a13d6b954748
AbrarTunio <abrar.tunio@yahoo.com>
2025-04-08 18:07:08 +0500

    conditional and arithmetic

---
+++ 20250408/arith.js
@@ -0,0 +1,46 @@
+// Arithmetic Operators
+
+// Plus + 
+// Minus - 
+// Division /
+// Multiplication *
+// Modulus - Remainder %
+// Power - Exponent **
+
+let num1 = 5
+let num2 = 0
+
+add = num1 + num2  //  console.log(add) ----> 17 
+sub = num1 - num2  //  13
+pro = num1 * num2  //  30
+div = num1 / num2  // 7.5
+mod = num1 % num2  //  0
+exp = num1 ** num2 //  5 * 5 => 5**2
+
+result = 2 ** 2 + 5 * (2 + 3) - 1
+
+value = 15 / 5
+
+// ------------------
+
+a = 5  // String
+b = 5    // Number
+
+// Conditional Operators
+
+a > b  // false
+a < b  // true
+a == b // true
+a === b  // false
+a != b // true
+
+// -------------------
+
+word1 = "w"
+
+word2 = "W"
+
+
+
+
+

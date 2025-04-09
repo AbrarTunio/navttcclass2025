82b96f1cc36c28dbdcd0b5564d5baa1d06d93e50
AbrarTunio <abrar.tunio@yahoo.com>
2025-04-09 16:32:10 +0500

    y

---
+++ 20250409/conditional.js
@@ -0,0 +1,18 @@
+// let myName = "Abrar Hussain
+
+let age = Number( prompt( "Enter Your age: " ) )
+
+console.log( typeof age )
+
+if (  age < 18 ) {
+ document.body.innerHTML = 
+  "You are too young to caste vote"
+} else if (  age === 18 ){
+ document.body.innerHTML = 
+  "Caste your vote thoughtfully"
+} else{
+ document.body.innerHTML = 
+  "Fankariyan choro age numebr me dalo!"
+}
+
+
